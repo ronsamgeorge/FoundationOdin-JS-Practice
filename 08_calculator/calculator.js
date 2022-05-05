0d00fdbd872b5782d3d053a7d3e7d1147aeacd2b
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-06 01:50:10 +0530

    Complete Ex : 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1]; 
 };
 
 const subtract = function() {
-	
+	return arguments[0] >= arguments[1] ? (arguments[0]-arguments[1]) : (arguments[1]-arguments[0]);
 };
 
 const sum = function() {
-	
+
+  const array = Array.from(arguments[0]);
+    
+	const finalResult = array.reduce((result,argument) => { 
+    result += argument;
+    return result
+  },0);
+
+  return finalResult;
 };
 
 const multiply = function() {
 
+  
+  const array = Array.from(arguments[0]);
+    
+	const finalResult = array.reduce((result,argument) => { 
+    result *= argument;
+    return result
+  },1);
+
+  return finalResult;
 };
 
 const power = function() {
-	
+	return arguments[0]**arguments[1]
 };
 
 const factorial = function() {
-	
+	let finalResult = 1;
+  for(let i = Number(arguments[0]); i>0; i--){
+    finalResult *= i;
+  }
+
+  return finalResult;
 };
+
 
 // Do not edit below this line
 module.exports = {
