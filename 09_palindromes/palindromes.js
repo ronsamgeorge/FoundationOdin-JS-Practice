edee9bcbe92ddf1f65fd319c26a96c285f6eb1ce
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-07 00:55:51 +0530

    Complete Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
 const palindromes = function () {
 
-};
+    let str = arguments[0];
+    str = str.replace(/[^\w\']|_/g, "");
+    console.log(str);
+    for(let i = 0, j=str.length-1; i<j; i++,j-- ){
+        if (str[i].toLowerCase() === str[j].toLowerCase()){
+            continue;
+        }else{
+            return false
+        }
+    }
 
+    return true;
+
+};
+console.log(palindromes("A car, a man, a maraca."));
 // Do not edit below this line
 module.exports = palindromes;
