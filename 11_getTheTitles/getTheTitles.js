d3864c1309c105503afca39af0d7b5ee3bea65bc
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-07 01:42:43 +0530

    Complete ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,29 @@
 const getTheTitles = function() {
 
+    const array = Array.from(arguments[0]);
+    console.log(array.length);
+
+    const titles = array.reduce((title,book)=>{
+        title.push(book.title);
+        return title
+    },[]);
+
+    return titles;
+
 };
+
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
