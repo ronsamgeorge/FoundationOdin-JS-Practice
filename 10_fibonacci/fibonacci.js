6aa6e12e7812d7f89d18cfa887adf066e519572a
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-07 01:18:01 +0530

    Complete Ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
 const fibonacci = function() {
+    const num = Number(arguments[0]);
+
+    if (num < 0){
+        return "OOPS";
+    }else if (num <=2){
+        return 1;
+    }else {
+        return fibonacci(num-1) + fibonacci(num-2); 
+    }
 
 };
+
+console.log(fibonacci('4'));
 
 // Do not edit below this line
 module.exports = fibonacci;
