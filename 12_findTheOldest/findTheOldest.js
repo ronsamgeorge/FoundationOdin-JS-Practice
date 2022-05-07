f57abefb3fe16738f3d8e37fbf67ced62ee63f9b
Ronald George <ronaldsamgeorge@gmail.com>
2022-05-08 00:19:17 +0530

    Solve Ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  
+  let oldest = "";
+  let lived = 0;
+  for(let i = 0; i<array.length; i++){
+    
+    let currentAge = 0;
+    if (!array[i].yearOfDeath){
+      currentAge = (new Date()).getFullYear() - array[i].yearOfBirth;
+    }else{
+      currentAge = array[i].yearOfDeath - array[i].yearOfBirth;
+    }
 
+    if(currentAge > lived){
+      lived = currentAge;
+      oldest = array[i];
+    }
+  }
+
+  return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
